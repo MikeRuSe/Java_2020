a0f146d573e4cedf79c398b8884bc4355af0ca56
Jorge <jorge.martinbenitogarcia@usp.ceu.es>
2020-11-22 17:09:16 +0100

    Catalogo 2

--- src/dominio/Catalogo.java
+++ src/dominio/Catalogo.java
@@ -4,9 +4,14 @@ import java.util.ArrayList;
 
 public class Libreta{
 	private ArrayList<Telefono> coleccionTelefonos = new ArrayList<>();
-	
+	private int linea_valor_modificar = catalogo.indexOf("Valor a modificar");
+
 	public void annadirTelefono(Telefono telefono){
 		coleccionTelefonos.add(telefono);
+	}
+
+	public void eliminarTelefono(Telefono telefono){
+		coleccionTelefonos.remove(telefono);
 	}
 
 	public String toString(){
