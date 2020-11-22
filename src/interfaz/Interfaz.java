8d2020e4705a05d70293179c3985fb314b83a0de
Mike <miguel.rubiosemelas.usp@ceu.es>
2020-11-22 17:21:11 +0100

    Interfaz

--- src/interfaz/Interfaz.java
+++ src/interfaz/Interfaz.java
@@ -2,52 +2,73 @@ package interfaz;
 
 import dominio.*;
 import java.util.Scanner;
-import java.io.*;
-import java.text.*;
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.FileWriter;
 
-public class Interfaz {
-	private static String HELP_TEXT = "Texto de ayuda";
-	private static String NOMBRE_FICHERO = "libretaDeContactos.txt";
-	public static void main(String[] args) {
-		Libreta libreta = inicializarLibreta(NOMBRE_FICHERO);
-		//procesar args[0]
-		if(args[0].equals("help")) {
+public class Interfaz{
+	private static String HELP = "Texto de ayuda";
+	private static String NOMBRE_FICHERO = "catalogoDeTelefonos.txt";
+
+	public static void procesado(String input){
+		String[] args = input.split(" ");
+		Catalogo catalogo = inicializarCatalogo(NOMBRE_FICHERO);
+		if(args[0].equals("help")){
 			System.out.println(HELP_TEXT);
-		} else if (args[0].equals("list")) {
-			System.out.println(libreta);
-		} else if (args[0].equals("add")) {
-			Contacto contacto = new Contacto(args[1], args[2]);
-			libreta.annadirContacto(contacto);
-			inicializarFichero(libreta);
+		} else if (args[0].equals("list")){
+			if(catalogo.toString().equals("")){
+				System.out.println("No hay ningún telefono en el catalogo");
+			} else {
+				System.out.println(catalogo);
+			}
+		} else if (args[0].equals("add")){
+			Telefono telefono = new Telefono(args[1], args[2]);
+			catalogo.annadirTelefono(telefono);
+			inicializarFichero(catalogo);
+		}
+		else if (args[0].equals("modify")){
+			int index = catalogo.indexOf(args[1]);
+			Telefono telefono = new Telefono(args[1], args[2], args[3], args[4]);
+			catalogo.modificarTelefono(index, telefono);
+			inicializarFichero(catalogo);
+		}
+		else if (args[0].equals("remove")){
+			int index = catalogo.indexOf(args[1]);
+			catalogo.eliminarTelefono(index);
+			inicializarFichero(catalogo);
 		}
 	}
-	private static void inicializarFichero(Libreta libreta) {
-		try {
+
+	private static void inicializarFichero(Catalogo catalogo){
+		try{
 			FileWriter fw = new FileWriter(NOMBRE_FICHERO);
-			fw.write(libreta.toString());
+			fw.write(catalogo.toString());
 			fw.close();
-		} catch (Exception e) {
+		} catch (Exception e){
 			System.out.println(e);
 		}
 	}
 
-	private static Libreta inicializarLibreta(String nombreFichero) {
-		Libreta libreta = new Libreta();
-		try {
-			Scanner sc = new Scanner(nombreFichero);
-			while(sc.hasNext()) {
-				String nombre = sc.next();
-				String nTel = sc.next();
-				Contacto contacto = new Contacto(nombre, nTel);
-				libreta.annadirContacto(contacto);
-			} 
+	private static Catalogo inicializarCatalogo(String nombreFichero){
+		Catalogo catalogo = new Catalogo();
+		try{
+			File file = new File(nombreFichero);
+			Scanner sc = new Scanner(file);
+			while(sc.hasNext()){
+				String modeloTelefono = sc.next();
+				String marcaTelefono = sc.next();
+				String precioTelefono = sc.next();
+				String descuentoTelefono = sc.next();
+				Telefono telefono = new Telefono(modeloTelefono, marcaTelefono, precioTelefono, descuentoTelefono);
+				catalogo.annadirTelefono(telefono);
+			}
 			sc.close();
-		} /*catch (FileNotFoundException e) {
-			//System.out.println(e);
-			System.out.println("No hay ningun contacto en la libreta");
-		}*/ catch (Exception e) {
+		} catch (FileNotFoundException e){
+			inicializarFichero(catalogo);
+		} catch (Exception e){
 			System.out.println(e);
 		}
-		return libreta;
+		return catalogo;
 	}
 }
+
