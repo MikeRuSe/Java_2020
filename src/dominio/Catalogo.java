9cfcf85826d9fd9ad48e9b8f6a382e2a99e317ab
Jorge <jorge.martinbenitogarcia@usp.ceu.es>
2020-12-01 18:46:28 +0100

    Catalogo borrado lineas de codigo comentadas y con javadoc

--- src/dominio/Catalogo.java
+++ src/dominio/Catalogo.java
@@ -7,31 +7,51 @@ import java.lang.StringBuilder;
 public class Catalogo{
 	private ArrayList<Telefono> coleccionTelefonos = new ArrayList<>();
 
+	/**
+	* Este metodo permite añadir un Telefono a la lista del catálogo,
+	*
+	* @param telefono nuevo en el catálogo
+	*/
+
 	public void annadirTelefono(Telefono telefono) {
 		coleccionTelefonos.add(telefono);
 	}
+
+	/**
+	* Este metodo permite eliminar un Telefono previamente añadido del la lista del catálogo,
+	*
+	* @param telefono eliminado del catálogo
+	*/
 
 	public void eliminarTelefono(String argumentos) {
 		for(int i = 0 ; i < coleccionTelefonos.size(); i++) {
         	Telefono nuevo_telefono = coleccionTelefonos.get(i);
         	if(argumentos.equals(nuevo_telefono.getModeloTelefono()))  {
-        		//System.out.println(argumentos);
-        		//System.out.println(index);
         		coleccionTelefonos.remove(i);
         	}
     	}
 	}
 
+	/**
+	* Este metodo permite modificar un Telefono de la lista del catálogo,
+	*
+	* @param telefono modificado del catálogo
+	*/
+
 	public void modificarTelefono(Telefono telefono, String argumentos) {
 		for(int i = 0 ; i < coleccionTelefonos.size(); i++) {
         	Telefono nuevo_telefono = coleccionTelefonos.get(i);
         	if(argumentos.equals(nuevo_telefono.getModeloTelefono()))  {
-        		//System.out.println(argumentos);
-        		//System.out.println(index);
         		coleccionTelefonos.set(i, telefono);
         	}
     	}
     }
+
+	/**
+	* Este metodo permite recoger los datos introducidos y los devuelve,
+	*
+	* @return datos del catálogo
+	*/
 
 	public String toString() {
 		StringBuilder datos = new StringBuilder();
