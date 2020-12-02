8798b8f74412430398c17b61b281032feb29fd9e
Mike <miguel.rubiosemelas.usp@ceu.es>
2020-12-02 10:21:18 +0100

    Principal.java

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -4,14 +4,6 @@ import interfaz.Interfaz;
 /*
  * Esta clase recibe argumentos por consola y los manda a interfaz para ejecutarlos
  * 
- * Los parámetros permitidos son:
- * - list
- * - remove
- * - add
- * - modify
- * - help
- * 
- * (Cualquier otro parámetro generará el error 127 'No se reconoce el parametro').
  *
  *@author: Jorge Martin-Benito Garcia, Xavier Alexander Mora Peraza, Dulibeth Medina Chirinos, Miguel Rubio Semelas
  * 
@@ -22,7 +14,15 @@ public class Principal{
 	/**
 	 * Toma como entrada unos argumentoss por consola
 	 * @param args Argumentos del programa 
-	 * */
+	 * Los parámetros permitidos son:
+         * - list
+         * - remove
+         * - add
+         * - modify
+         * - help
+         * 
+         * (Cualquier otro parámetro generará el error 127 'No se reconoce el parametro'). 
+        */
         public static void main(String[] args){
                 String input = "";
                 for(int i = 0; i< args.length; i++){
