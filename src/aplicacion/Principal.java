1a6925075a3a424c250a2ba1b3515cf77876c3c8
Dulibeth <dulibeth.medinachirinos@usp.ceu.es>
2020-12-09 09:49:11 +0100

    Javadoc actualizado Principal.java

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,13 +1,13 @@
 package aplicacion;
 
 import interfaz.Interfaz;
-/*
+/**
  * Esta clase recibe argumentos por consola y los manda a interfaz para ejecutarlos
  * 
  *
- * @author: Jorge Martin-Benito Garcia, Xavier Alexander Mora Peraza, Dulibeth Medina Chirinos, Miguel Rubio Semelas
+ * @author Jorge Martin-Benito Garcia, Xavier Alexander Mora Peraza, Dulibeth Medina Chirinos, Miguel Rubio Semelas
  * 
- * @version: 1.2.14.12220
+ * @version 1.2.14.12220
  */
 public class Principal{
 
